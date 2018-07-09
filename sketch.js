6cbb0fe3c056a3f53decd9dc3a3067288e65b03c
Guillermo <guilleeh@uci.edu>
2018-07-09 02:09:41 -0700

    Added js for buttons

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 const container = document.querySelector('.box');
-
+var startingSize = 16;
 function getRandomColor() {
 	var letter = '0123456789ABCDEF';
 	var color = '#';
@@ -10,52 +10,102 @@ function getRandomColor() {
 }
 
 //create board
-for( let i = 0; i < 15; i++ ) {
-	let row_container = document.createElement('div');
-	row_container.classList.add('column');
-	for ( let j = 0; j < 15; j++ ) {
-		let content = document.createElement('div');
-		content.classList.add('row');
-		row_container.appendChild(content);
+function createBoard(size) {
+	for( let i = 0; i < size; i++ ) {
+		let row_container = document.createElement('div');
+		row_container.classList.add('column');
+		for ( let j = 0; j < size; j++ ) {
+			let content = document.createElement('div');
+			content.classList.add('row');
+			row_container.appendChild(content);
+		}
+		container.appendChild(row_container);
 	}
-	container.appendChild(row_container);
 }
 
+function removeBoard(size){
+	var firstChildren = document.querySelector('.box');
+	while( firstChildren.firstChild ) {
+		var secondChildren = firstChildren.children;
+		while( secondChildren.firstChild ) {
+			console.log(firstChildren);
+			secondChildren.removeChild(secondChildren.firstChild);
+			// console.log(secondChildren.children[j]);
+			// console.log(secondChildren.children[j]);
+			//secondChildren.removeChild(secondChildren.childNodes[j]);
+		}
+		//firstChildren[i].remove();
+		firstChildren.removeChild(firstChildren.firstChild);
+	}
+}
+
+function resetBoardColors(size){
+	let allChildren = document.querySelector('.box');
+	let allInnerChildren = allChildren.querySelectorAll('.column');
+	console.log(allInnerChildren[0].childNodes);
+	for( let i = 0; i < size; i++ ) {
+		for( let j = 0; j < size; j++ ) {
+			allInnerChildren[i].childNodes[j].style.backgroundColor = "#6F9FD8";
+		}
+	}
+}
 
 //add left border
-let allBoxes = document.querySelector('.box').children;
-let boxList = allBoxes[0].children;
-console.log(boxList);
-let listLength = boxList.length;
+function addBorders(changeSize = 0) {
+	let allBoxes = document.querySelector('.box').children;
+	let boxList = allBoxes[0].children;
+	let listLength = boxList.length;
 
-for( let i = 0; i < listLength; i++ ) {
- 	boxList[i].classList.add("left-border");
-	// boxList[i].classList.add("change-width");
-}
 
-//right border
-let lastChild = document.querySelector('.box').lastChild;
-let elementListLast = lastChild.querySelectorAll('.row');
-let elementListLastLength = elementListLast.length;
+	for( let i = 0; i < listLength; i++ ) {
+	 	boxList[i].classList.add("left-border");
+		// boxList[i].classList.add("change-width");
+	}
 
-for( let i = 0; i < elementListLastLength; i++ ) {
- 	elementListLast[i].classList.add("right-border");
-}
+	//right border
+	let lastChild = document.querySelector('.box').lastChild;
+	let elementListLast = lastChild.querySelectorAll('.row');
+	let elementListLastLength = elementListLast.length;
 
-//bottom, top border
-let allChildren = document.querySelector('.box');
-let allInnerChildren = allChildren.querySelectorAll('.column');
-let allInnerLength= allInnerChildren.length;
-for( let i = 0; i < allInnerLength; i++ ) {
-	allInnerChildren[i].firstChild.classList.add('top-border');
-	allInnerChildren[i].lastChild.classList.add('bottom-border');
-	
-	allInnerChildren[i].onmouseover = function(e) {
-		// console.log(e.target);
-		// console.log(getRandomColor());
-		e.target.style.backgroundColor = getRandomColor();
+	for( let i = 0; i < elementListLastLength; i++ ) {
+	 	elementListLast[i].classList.add("right-border");
+	}
+
+	//bottom, top border
+	let allChildren = document.querySelector('.box');
+	let allInnerChildren = allChildren.querySelectorAll('.column');
+	let allInnerLength= allInnerChildren.length;
+	for( let i = 0; i < allInnerLength; i++ ) {
+		allInnerChildren[i].firstChild.classList.add('top-border');
+		allInnerChildren[i].lastChild.classList.add('bottom-border');
+		allInnerChildren[i].onmouseover = function(e) {
+			// console.log(e.target);
+			// console.log(getRandomColor());
+			e.target.style.backgroundColor = getRandomColor();
+		}
 	}
 }
 
+//Change size
+
+let boardSize = document.querySelector(".size-button");
+boardSize.addEventListener('click', function(){
+	removeBoard(startingSize);
+	startingSize = prompt("Please enter your board size (Limit: 2 <= size <= 20 )");
+	while( (startingSize < 2 || startingSize > 20) && startingSize != null ){
+		startingSize = prompt("Please enter your board size (Limit: 2 <= size <= 20 )");
+	}
+	createBoard(startingSize);
+	addBorders(startingSize);
+})
+
+//reset colors
+let resetColor = document.querySelector(".reset-button");
+resetColor.addEventListener('click', function(){
+	resetBoardColors(startingSize);
+});
+
+createBoard(startingSize);
+addBorders();
 
 
